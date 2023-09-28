bf6dde72f58b5f57892a1b48ef1cac612e7704f6
lavanyakottapalli <lavanyakottapalli96@gmail.com>
2023-09-27 20:54:21 -0700

    Add search

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,52 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+// <copyright file="Program.cs" company="Microsoft Corp.">
+// Copyright (c) Microsoft Corp.. All rights reserved.
+// </copyright>
 
-app.MapGet("/", () => "Hello World!");
+namespace Microsoft.Movie.Store
+{
+    using Microsoft.AspNetCore;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.Hosting;
+    using Microsoft.Extensions.Logging;
 
-app.Run();
+    /// <summary>
+    /// Program.
+    /// </summary>
+#pragma warning disable CA1052
+    public class Program
+#pragma warning restore CA1052
+    {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        /// <summary>
+        /// Configure host builder properties.
+        /// </summary>
+        /// <param name="args">args.</param>
+        /// <returns>IWebHost Builder.</returns>
+        public static IWebHostBuilder CreateHostBuilder(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
+                .ConfigureLogging(loggingBuilder =>
+                {
+                    _ = loggingBuilder.AddConsole();
+                })
+            .ConfigureAppConfiguration((context, config) =>
+            {
+                if (context.HostingEnvironment.IsDevelopment())
+                {
+                    _ = config.AddUserSecrets<Program>();
+                }
+            })
+            .UseUrls(urls: "http://localhost:5008")
+            .UseStartup<Startup>();
+        }
+    }
+}
---
+++ src/Startup.cs
@@ -0,0 +1,84 @@
+﻿// <copyright file="Startup.cs" company="Microsoft Corp.">
+// Copyright (c) Microsoft Corp.. All rights reserved.
+// </copyright>
+
+namespace Microsoft.Movie.Store
+{
+    using Azure.Search.Documents;
+    using Azure;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using RestSharp;
+
+    /// <summary>
+    /// StartUp.
+    /// </summary>
+    public class Startup
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Startup"/> class.
+        /// </summary>
+        /// <param name="configuration">The configuration.</param>
+        /// <param name="environment">The hosting environment.</param>
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
+        {
+            this.Configuration = configuration;
+            this.Environment = environment;
+        }
+
+        /// <summary>
+        /// Gets the configuration.
+        /// </summary>
+        public IConfiguration Configuration { get; }
+
+        /// <summary>
+        /// Gets the environment.
+        /// </summary>
+        public IWebHostEnvironment Environment { get; }
+
+        /// <summary>
+        /// Configures the services. This method gets called by the runtime. Use this method to add services to the container.
+        /// </summary>
+        /// <param name="services">The services.</param>
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            string indexName = "movie-index";
+
+            // Get the service endpoint and API key from the environment
+            Uri endpoint = new Uri("https://movie-store.search.windows.net");
+            string key = "";
+
+            // Create a client
+            AzureKeyCredential credential = new AzureKeyCredential(key);
+            SearchClient client = new SearchClient(endpoint, indexName, credential);
+
+            services.AddSingleton(client);
+
+            RestClientOptions options = new RestClientOptions("https://api.themoviedb.org/3/authentication");
+
+#pragma warning disable CA2000 // Dispose objects before losing scope
+            RestClient client = new RestClient(options);
+#pragma warning restore CA2000 // Dispose objects before losing scope
+            RestRequest request = new RestRequest("");
+            request.AddHeader("accept", "application/json");
+            request.AddHeader("Authorization", "Bearer ");
+
+            services.AddSingleton(client);
+        }
+
+        /// <summary>
+        /// Configures the specified application. This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// </summary>
+        public void Configure(IHostApplicationLifetime lifetime, RestClient restClient)
+        {
+            ArgumentNullException.ThrowIfNull(lifetime);
+
+            _ = lifetime.ApplicationStopping.Register(() =>
+            {
+                restClient.Dispose();
+            });
+        }
+    }
+}
